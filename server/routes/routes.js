f0955589d76142dc792fd67632a57d0681d513ce
Shen Liu <slsteven@bu.edu>
2024-03-23 02:06:44 -0400

    added user regist and login endpoint in server

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 // import the model
 const Model = require('../model/model');
+// import model for user
+const User = require("../model/userModel");
 // import api for converting pdf-to-docx
 var convertapi = require('convertapi')('sqnUSLzN2sMxQu0v');
 // for handling file uploads
@@ -11,6 +13,10 @@ const multer = require('multer');
 const path = require('path');
 // for handling file delete
 const fs = require('fs');
+// for hashing hash the password
+const bcrypt = require("bcrypt");
+// for generating token
+const jwt = require("jsonwebtoken");
 
 
 
@@ -27,7 +33,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 
-//Post Method
+//Post Method for pdf-to-docx
 router.post('/pdf-to-docx', upload.single('file'), async (req, res) => {
     try {
 
@@ -39,7 +45,9 @@ router.post('/pdf-to-docx', upload.single('file'), async (req, res) => {
 
          // Perform the conversion
          
-         convertapi.convert('docx', {File: filePath}, 'pdf').then(async (result) => {
+         convertapi
+            .convert('docx', {File: filePath}, 'pdf')
+            .then(async (result) => {
             // ensure save the data to the database is executed only after the file conversion is complete and convertedFilePath is set
             const convertedFilePath = result.file.url;
 
@@ -78,6 +86,107 @@ router.post('/pdf-to-docx', upload.single('file'), async (req, res) => {
     }
     
 })
+
+
+// post endpoint for register
+router.post("/register", (request, response) => {
+    // hash the password
+    bcrypt
+      .hash(request.body.password, 10)
+      .then((hashedPassword) => {
+        // create a new user instance and collect the data
+        const user = new User({
+          email: request.body.email,
+          password: hashedPassword,
+        });
+  
+        // save the new user
+        user
+          .save()
+          // return success if the new user is added to the database successfully
+          .then((result) => {
+            response.status(200).send({
+              message: "User Created Successfully",
+              result,
+            });
+          })
+          // catch error if the new user wasn't added successfully to the database
+          .catch((error) => {
+            response.status(500).send({
+              message: "Error creating user",
+              error,
+            });
+          });
+      })
+      // catch error if the password hash isn't successful
+      .catch((e) => {
+        response.status(500).send({
+          message: "Password was not hashed successfully",
+          e,
+        });
+      });
+});
+
+
+// post endpoint for login
+router.post("/login", (request, response) => {
+    // check if email exists
+    User.findOne({ email: request.body.email })
+  
+      // if email exists
+      .then((user) => {
+        // compare the password entered and the hashed password found
+        bcrypt
+          .compare(request.body.password, user.password)
+  
+          // if the passwords match
+          .then((passwordCheck) => {
+  
+            // check if password matches
+            if(!passwordCheck) {
+              return response.status(400).send({
+                message: "Passwords does not match",
+                error,
+              });
+            }
+  
+            //   create JWT token
+            const token = jwt.sign(
+              {
+                userId: user._id,
+                userEmail: user.email,
+              },
+              "RANDOM-TOKEN",
+              { expiresIn: "24h" }
+            );
+  
+            //   return success response
+            response.status(200).send({
+              message: "Login Successful",
+              email: user.email,
+              token,
+            });
+          })
+          // catch error if password does not match
+          .catch((error) => {
+            response.status(400).send({
+              message: "Passwords does not match",
+              error,
+            });
+          });
+      })
+      // catch error if email does not exist
+      .catch((e) => {
+        response.status(404).send({
+          message: "Email not found",
+          e,
+        });
+      });
+  });
+  
+
+
+
 
 //Get all Method
 router.get('/getAll', (req, res) => {
