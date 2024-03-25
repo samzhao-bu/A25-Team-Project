652a40a2fb0823f5fd8d3b6dfac3a26c4978eb7a
Shen Liu <slsteven@bu.edu>
2024-03-24 20:46:07 -0400

    added username in user database shcema

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, "Please provide name!"],
+      unique: [false, "name Exist"],
+    },
+
     email: {
         type: String,
         required: [true, "Please provide an Email!"],
