f9fbe3debd0d1b16bfc2e3c3191aa0e9874f6015
Shen Liu <slsteven@bu.edu>
2024-04-24 15:25:02 -0400

    added some text on home page

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -3,7 +3,12 @@
 const Dashboard = () => {
     return (
         <div>
-            <h1>dashboard.</h1>
+            <h1>One-Stop Digital File Hub</h1>
+            <h2>We provide features:</h2>
+            <h2> Seamless File Conversion</h2>   
+            <h2> Cross-Language File Translator</h2>   
+            <h2> Intelligent File Summary</h2> 
+            <h1>Try it out and enjoy!</h1>
         </div>
     );
 };
